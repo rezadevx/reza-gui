18a64e6ddc4d979a55144451762bee689d36c4a9
rezadevs <sawe57192@gmail.com>
2026-07-21 05:36:53 +0700

    clean: remove decorative symbols, # and annotations from elements/Divider.lua

--- elements/Divider.lua
+++ elements/Divider.lua
@@ -1,14 +1,14 @@
 return function(Library, Theme, U)
-    return function(Sec, items)
-        function Sec:Divider()
-            U.New("Frame", {
-                BackgroundColor3       = Theme.Border,
-                BorderSizePixel        = 0,
-                Size                   = UDim2.new(1, 0, 0, 1),
-                BackgroundTransparency = 0.35,
-                ZIndex                 = 16,
-                Parent                 = items,
-            })
-        end
-    end
+ return function(Sec, items)
+ function Sec:Divider()
+ U.New("Frame", {
+ BackgroundColor3 = Theme.Border,
+ BorderSizePixel = 0,
+ Size = UDim2.new(1, 0, 0, 1),
+ BackgroundTransparency = 0.35,
+ ZIndex = 16,
+ Parent = items,
+ })
+ end
+ end
 end
